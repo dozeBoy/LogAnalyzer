ae1ef7d87cf3eac0dcd9b5450091740dc0fff139
inspiringinovation <vasincaalex@gmail.com>
2014-07-07 11:36:16 +0300

    Further explanations

--- LogAnalyzerV2/src/com/saguaro/LogAnalyzer/Heap.java
+++ LogAnalyzerV2/src/com/saguaro/LogAnalyzer/Heap.java
@@ -3,74 +3,123 @@ package com.saguaro.LogAnalyzer;
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
+
+//  a heap may be a better and simpler implementation because it use less memory overhead 
+//  as we store the data directly in the array
+//	at it worst case it still has a complexity of O(log N) beside
+//  a balanced tree that in the worst case scenario has a complexity of O(n)
+//  the number of elements in the heap should be equal to the number of log files
+
 public class Heap<T extends Comparable<T>> {
 
-	private ArrayList<T> items;// use arrayList so items can be added without
-								// limit
+	/*
+	 * When we refer to an array (or a sub array) of size size as a "heap", 
+	 * we mean that it has the heap property. 
+	 * For all i, 0 <= i < size: 
+	 * if (left(i) < size) 
+	 *      order.compare(heap[i], heap[left(i)]) >= 0. 
+	 * if (right(i) < size)
+	 *      order.compare(heap[i], heap[right(i)]) >= 0.
+	 */
 
+	private ArrayList<T> items; // use arrayList so items can be added without
+								// limit
+								// we go thorough the heap,top the bottom,level by level and in each level 
+								// we go left to right adding each item to the end of the array
+
+	
+	/*
+	 * The default initial capacity for the Heap
+	 */
+	
 	public Heap() {
 		items = new ArrayList<T>();// set up the arrayList to default initial
 									// capacity
 	}
 
-	// make the method private so it can be called only from the insert process
-	private void siftUp() {
-		int k = items.size() - 1;   // set k to the index of the last element of
-									// the ArrayList
-									// because is the one that is going to be
-									// sifted up
+	/* 
+	 *  Reach heap property : Propagate up from last element.
+	 */
+	
+	private void swapUp() {
+		int k = items.size() - 1; // set k to the index of the last element of
+								  // the ArrayList
+								  // because is the one that is going to be
+								  // swap up
 
-		while (k > 0) { // loop until the sift up item reaches the top of the
-						// heap
+		//Keep swaping until we reach the top of the heap or find a larger parent
+		while (k > 0) { 
 			int p = (k - 1) / 2;    // compute parent,p, item
 									// we know this because k has children at
-									// indexes 2*k+1 (left child) and 2*k+2 (right child)
-									// so to find the index of the child we consider p parent and 2*p+1 being the left child
-									// we set the index of the child to k so p = (k - 1)/2
+									// indexes 2*k+1 (left child) and 2*k+2
+									// (right child)
+									// so to find the index of the child we
+									// consider p parent and 2*p+1 being the
+									// left child
+									// we set the index of the child to k so p =
+									// (k - 1)/2
 
 			T item = items.get(k);
 			T parent = items.get(p);
-			if (item.compareTo(parent) > 0) { //compare that the k item is greater that the p item
-											  //if so, swap
+			
+			
+			// compare that the k item is
+			// greater that the p item
+			// if so, swap child with parent
+			
+			if (item.compareTo(parent) > 0) { 
 				items.set(k, parent);
 				items.set(p, item);
-
+		
 				
-				k = p; //move k up one level
+				// move k up one level
+				k = p; 
 			} else {
 				break;
 			}
 		}
 	}
-
+	
+	/*
+	 * Add new item to the end of the ArrayList 
+	 */
+	
 	public void insert(T item) {
-		items.add(item); // add new item to the end of the ArrayList
-		siftUp();
+		items.add(item); // 
+		swapUp(); 
 	}
 
-	// declare the method as private so it can be called only from the delete
-	// process
-	private void siftDown() {
+
+	private void swapDown() {
 		int k = 0;
 		int l = 2 * k + 1;
-		while (l < items.size()) { //loop as long as the left child index is inside the ArrayList 
-								   //were the heap elements are stored
+		
+		
+		while (l < items.size()) {  // loop as long as the left child index is
+									// inside the ArrayList
+									// were the heap elements are stored
+
+			int max = l, r = l + 1; // set max to l index and r to l+1 because l
+									// is already set 2*k+1
 			
-			int max = l, r = l + 1; 		//set max to l index and r to l+1 because l is already set 2*k+1
-			if (r < items.size()) { 		//loop to check if there is a right child
-				if (items.get(r).compareTo(items.get(l)) > 0) { //compare right and left children
-					max++;					//if right child is greater increment max
-											//max was previously set to l
+			if (r < items.size()) { // loop to check if there is a right child
+				if (items.get(r).compareTo(items.get(l)) > 0) { // compare right
+																// and left
+																// children
+					max++; // if right child is greater increment max
+							// max was previously set to l
 				}
 			}
-			if (items.get(k).compareTo(items.get(max)) < 0) {//compare k item to max item
-															 //if k is less then swap	
-					
-				//swap it
+			if (items.get(k).compareTo(items.get(max)) < 0) {// compare k item
+																// to max item
+																// if k is less
+																// then swap
+
+				// swap it
 				T temp = items.get(k);
 				items.set(k, items.get(max));
 				items.set(max, temp);
-				k = max; //set k to max
+				k = max; // set k to max
 				l = 2 * k + 1; //
 			} else {
 				break;
@@ -92,7 +141,7 @@ public class Heap<T extends Comparable<T>> {
 		items.set(0, items.remove(items.size() - 1));// the last item is removed
 														// and written in the
 														// first position
-		siftDown(); // call siftdown
+		swapDown(); // call swap down
 		return hold;
 	}
 
